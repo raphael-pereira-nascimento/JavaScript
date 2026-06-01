b258801b16175473a24e1a238a3b65cfe2c19686
lphfarianunes3-design <raphael.nascimento.dev@gmail.com>
2026-06-01 08:29:44 -0300

    add tipos de dados number

---
+++ tipos-de-dados.js
@@ -0,0 +1,7 @@
+let idade = 25; //Número inteiro
+let altura = 1.75;//Número de ponto flutuante
+let temperatura = -5;//Número negativo
+
+console.log(idade)
+console.log(altura)
+console.log(temperatura)
