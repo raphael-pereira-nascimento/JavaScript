ee3f0b48d1b37b26fab7a64e85f9bb32da65acca
marina-arruda <raphael.nascimento.dev@gmail.com>
2026-07-27 08:52:51 -0400

    add exemplo de if

---
+++ if.js
@@ -0,0 +1,8 @@
+let hora = 15;
+
+if (hora < 12) {
+    console.log("Bom dia");
+}
+
+// O restante do código continua independente se a condição for verdadeiro ou não.
+console.log("Dinheiro não traz felicidade. Me dê o seu e seja felil!")
